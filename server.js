80459e0cd74c37505e65eee53ec47021552e06c0
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-06-02 08:43:48 -0500

    refactor: restructure server startup and add global CORS support

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const sequelize = require('./config/database');
 require('dotenv').config();
@@ -8,18 +9,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors()); 
 
 app.use('/auth', authRoutes);
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
-sequelize.authenticate()
-  .then(() => {
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
     console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`User Login Service running on port ${PORT}`);
-    });
-  })
-  .catch(err => {
+    console.log(`User Login Service running on port ${PORT}`);
+  } catch (err) {
     console.error('Unable to connect to database:', err);
-  });
+  }
+});
